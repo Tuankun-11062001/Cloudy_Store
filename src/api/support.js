bb248b6bf385fa9e785a64229e976776b1777a74
June <nhoxtuanhero1@gmail.com>
2024-04-13 10:17:20 +0700

    fix support api

--- src/api/support.js
+++ src/api/support.js
@@ -1,6 +1,19 @@
 export default async function handleCreateSupport(payload) {
   try {
     // Gọi API từ server của bạn
+    // const response = await fetch(`http://localhost:3001/support/create`, {
+    //   method: "POST",
+    //   mode: "cors", // no-cors, *cors, same-origin
+    //   cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    //   credentials: "same-origin", // include, *same-origin, omit
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //     // 'Content-Type': 'application/x-www-form-urlencoded',
+    //   },
+    //   redirect: "follow", // manual, *follow, error
+    //   referrerPolicy: "no-referrer",
+    //   body: JSON.stringify(payload),
+    // });
     const response = await fetch(`http://localhost:3001/support/create`, {
       method: "POST",
       mode: "cors", // no-cors, *cors, same-origin
@@ -14,7 +27,6 @@ export default async function handleCreateSupport(payload) {
       referrerPolicy: "no-referrer",
       body: JSON.stringify(payload),
     });
-    //   const response = await fetch(`https://twin-s.vercel.app/product`);
     const data = await response.json();
 
     return data;
